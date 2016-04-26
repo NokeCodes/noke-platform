87b8ca92c880bf7999a0bf8e6b1cee72330d6189
Logan Glickfield <lsglick@users.noreply.github.com>
2016-04-26 14:29:50 -0400

    Add Django skeleton.

---
+++ nokecodes/settings/prod.py
@@ -0,0 +1,18 @@
+"""Settings for the production environment."""
+
+from base import *
+
+import os
+
+
+DEBUG = False
+
+
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ['RDS_DB_NAME'],
+    'USER': os.environ['RDS_USERNAME'],
+    'PASSWORD': os.environ['RDS_PASSWORD'],
+    'HOST': os.environ['RDS_HOSTNAME'],
+    'PORT': os.environ['RDS_PORT'],
+}
