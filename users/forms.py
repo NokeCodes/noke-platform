a83de102047a659992660e09739d675c165ece09
Logan Glickfield <lsglick@users.noreply.github.com>
2016-06-02 22:21:38 -0400

    Add a signup form.

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,18 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 from users.models import UserProfile
+
+
+class NewUserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(NewUserForm, self).__init__(*args, **kwargs)
+        self.fields['email'].required = True
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
 
 class UserForm(forms.ModelForm):
--- users/views.py
+++ users/views.py
@@ -1,15 +1,34 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, HttpResponseRedirect
 
-from .forms import UserForm, UserProfileForm
+from .forms import NewUserForm, UserForm, UserProfileForm
 
 
 def index(request):
     users = User.objects.all().select_related('profile').order_by('username')
     return render(request, 'users/users_list.html', {
         'users': users,
+    })
+
+
+def signup(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('users:profile', args=(request.user.get_username(),)))
+    if request.method == 'POST':
+        user_form = NewUserForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            user = authenticate(username=user_form.cleaned_data.get('username'),
+                                password=user_form.cleaned_data.get('password1'))
+            login(request, user)
+            return HttpResponseRedirect(reverse('users:edit_profile', args=(user.get_username(),)))
+    else:
+        user_form = NewUserForm()
+    return render(request, 'registration/signup.html', {
+        'user_form': user_form,
     })
 
 
